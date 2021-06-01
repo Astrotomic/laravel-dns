2d797d16adeb7b4bb62e8b10eced2ee775c68dee
Tom Witkowski <dev.gummibeer@gmail.com>
2021-06-01 12:26:27 +0200

    make main objects macroable

--- src/Dns.php
+++ src/Dns.php
@@ -3,12 +3,15 @@
 namespace Astrotomic\Dns;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Traits\Macroable;
 use Spatie\Dns\Dns as SpatieDns;
 use Spatie\Dns\Support\Domain;
 use Stringable;
 
 class Dns extends SpatieDns
 {
+    use Macroable;
+
     public function records(
         string | Domain | Stringable $search,
         int | string | array $types = DNS_ALL
--- src/Domain.php
+++ src/Domain.php
@@ -6,12 +6,15 @@ use Astrotomic\Dns\Casts\DomainCast;
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Traits\Macroable;
 use JsonSerializable;
 use Spatie\Dns\Support\Domain as SpatieDomain;
 use Stringable;
 
 class Domain extends SpatieDomain implements Castable, JsonSerializable, Jsonable
 {
+    use Macroable;
+
     public static function make(string|Stringable $domain): static
     {
         return new static($domain);
